2277c3e33fad8e4821d30a9a2708ec08909038d0
rishavemail786 <rishavemail786@gmail.com>
2022-08-11 14:27:04 +0530

    SOLUTION DONE

--- src/main/java/com/niit/jap/FurnitureItem.java
+++ src/main/java/com/niit/jap/FurnitureItem.java
@@ -1,2 +1,32 @@
-package org.example;public class FurnitureItem {
+package com.niit.jap;
+
+public class FurnitureItem {
+    String furnitureCode;
+    String furnitureType;
+    String colour;
+    String furnitureUsage;
+    int gradeOfFurniture;
+    int price;
+
+    FurnitureItem() {
+        furnitureUsage="";
+        furnitureType="";
+        furnitureCode="";
+        gradeOfFurniture=0;
+        price=0;
+
+    }
+    public double calculateDiscount(){
+        double discountPrice = 0;
+        if (furnitureType.equals("outdoor")){
+            //price*discount/100
+            discountPrice=(price*5)/100;
+            return price-discountPrice;
+        }
+        return discountPrice;
+
+
+    }
+
+
 }
--- src/main/java/com/niit/jap/FurnitureItemIMPL.java
+++ src/main/java/com/niit/jap/FurnitureItemIMPL.java
@@ -1,7 +1,28 @@
-package org.example;
+package com.niit.jap;
 
-public class Main {
+import java.util.Scanner;
+
+public class FurnitureItemIMPL {
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = scanner.nextLine();
+        FurnitureItem furnitureItem2 = new FurnitureItem();
+        System.out.println("enter furniture colour");
+        furnitureItem2.colour= scanner.nextLine();
+        System.out.println("what type of furniture you want indoor or outdoor ");
+        furnitureItem2.furnitureType=scanner.nextLine();
+        System.out.println("we have 3 grade enter your grade");
+        furnitureItem2.gradeOfFurniture=scanner.nextInt();
+        System.out.println("enter code if you know");
+        furnitureItem2.furnitureCode=scanner.nextLine();
+        System.out.println("enter your usage");
+        furnitureItem2.furnitureUsage=scanner.nextLine();
+        System.out.println("enter your budget price");
+        furnitureItem2.price=scanner.nextInt();
+
+        System.out.println("your discount price   =  "+furnitureItem2.calculateDiscount());
+
 
     }
 }
